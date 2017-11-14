5e96c204aa59c531c17a934722f37ae22521ecec
Ginger-13 <shepardcommander13.0@gmail.com>
2017-11-15 01:27:47 +0300

    header

---
+++ js/main.js
@@ -0,0 +1,11 @@
+/*$('.on-top').on('click', function() {
+    $('html, body').animate({
+        scrollTop: 0
+    }, 2000);
+
+    return false;
+});*/
+
+$('a[href="#"]').on('click', function() {
+    return false;
+});
