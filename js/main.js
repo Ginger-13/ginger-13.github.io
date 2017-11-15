5b5cd1b65599541b206d801fbbc0680190ca541d
Ginger-13 <shepardcommander13.0@gmail.com>
2017-11-15 18:55:31 +0300

    resp carousels

--- js/main.js
+++ js/main.js
@@ -13,11 +13,37 @@ $('a[href="#"]').on('click', function() {
 $('.pickup__carousel').slick({
     prevArrow: "",
     nextArrow: "<a class='arrow' href='#'><img src='/img/svg/arrow.svg'></a>",
-    slidesToShow: 3
+    slidesToShow: 3,
+    responsive: [
+        {
+            breakpoint: 1139,
+            settings: {
+                slidesToShow: 2
+            }
+        },
+        {
+            breakpoint: 767,
+            settings: {
+                slidesToShow: 1,
+                dots: true,
+                arrows: false
+            }
+        }
+    ]
 });
 
 $('.reviews__wrapper').slick({
     prevArrow: "<a class='arrow--left' href='#'><img src='/img/svg/arrow.svg'></a>",
     nextArrow: "<a class='arrow' href='#'><img src='/img/svg/arrow.svg'></a>",
-    slidesToShow: 2
+    slidesToShow: 2,
+    responsive: [
+        {
+            breakpoint: 767,
+            settings: {
+                slidesToShow: 1,
+                dots: true,
+                arrows: false
+            }
+        }
+    ]
 });
