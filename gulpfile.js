8dec01a1719bb8f966435a9456a3e9eafe5753b9
Ginger-13 <shepardcommander13.0@gmail.com>
2017-11-15 18:10:00 +0300

    inline svg

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ var minifyCSS    = require('gulp-minify-css');
 var postcss      = require('gulp-postcss');
 var autoprefixer = require('autoprefixer');
 var browserSync  = require('browser-sync').create();
+var svg          = require('gulp-inject-svg');
 
 
 gulp.task('css', function() {
@@ -26,6 +27,13 @@ gulp.task('serve', ['css'], function() {
 
     gulp.watch('./sass/*.sass', ['css']);
     gulp.watch('./sass/**/*.sass', ['css']);
-    gulp.watch('*.html').on('change', browserSync.reload);
+    gulp.watch('./html/*.sass', ['svg']);
     gulp.watch('./js/*.js').on('change', browserSync.reload);
+});
+
+var injectSvgOptions = { base: './' };
+gulp.task('svg', function() {
+    return gulp.src('./html/*.html')
+        .pipe(svg(injectSvgOptions))
+        .pipe(gulp.dest('./'));
 });
